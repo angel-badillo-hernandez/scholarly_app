ca037956046f79a1429303da340b04b878f975a8
Angel Badillo <badilloa022402@gmail.com>
2024-02-28 20:03:54 -0600

    Setting up project repo.

--- ScholarlyMainForm.cs
+++ ScholarlyMainForm.cs
@@ -1,8 +1,8 @@
 namespace scholarly_app
 {
-    public partial class Form1 : Form
+    public partial class ScholarlyMainForm : Form
     {
-        public Form1()
+        public ScholarlyMainForm()
         {
             InitializeComponent();
         }
