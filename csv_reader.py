d9275db828917bb84fbc7063019c5412ea3bd414
Angel Badillo <badilloa022402@gmail.com>
2024-03-04 11:42:35 -0600

    added example input data

--- csv_reader.py
+++ csv_reader.py
@@ -3,7 +3,12 @@ This is what we use to read in info from a CSV and covert it
 into Python objects, a list of tuples
 """
 import csv
+from student_record import StudentRecord
 
 class CSVReader:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, file_name:str) -> None:
+        self.file_name:str = file_name
+    
+    def read_values(self)->list[StudentRecord]:
+        with open(self.file_name, "r") as file:
+            pass
