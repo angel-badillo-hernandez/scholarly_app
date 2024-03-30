0730c881edeb5f0d65587856b0c40054c0da86ac
Angel Badillo <badilloa022402@gmail.com>
2024-03-30 12:39:10 -0500

    working on other tabs

---
+++ scholarly_manage_scholarships_tab.py
@@ -0,0 +1,41 @@
+from PyQt6.QtWidgets import (
+    QWidget,
+    QTabWidget,
+    QFormLayout,
+    QLayout,
+    QBoxLayout,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLineEdit,
+    QPushButton,
+    QToolButton,
+    QToolTip,
+    QGroupBox,
+    QComboBox,
+    QLabel,
+)
+from PyQt6.QtGui import QAction, QDoubleValidator, QValidator, QIntValidator, QIcon
+from typing import Callable
+import os
+
+BASE_DIR: str = os.path.dirname(__file__)
+
+# Function that takes no parameters, and returns nothing
+voidCallBack: Callable[[], None] = lambda: None
+
+
+class ScholarlyManageScholarshipsTab(QTabWidget):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Main layout for widget
+        main_layout: QVBoxLayout = QVBoxLayout()
+
+
+if __name__ == "__main__":
+    from PyQt6.QtWidgets import QApplication
+
+    a = QApplication([])
+    s = ScholarlyManageScholarshipsTab()
+    s.show()
+    a.exec()
--- scholarly_tab_bar.py
+++ scholarly_tab_bar.py
@@ -16,6 +16,10 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QAction, QDoubleValidator, QValidator, QIntValidator, QIcon
 from typing import Callable
 from scholarly_scholarship_tab import ScholarlyScholarshipTab
+from scholarly_manage_scholarships_tab import ScholarlyManageScholarshipsTab
+from scholarly_outstanding_student_awards_tab import (
+    ScholarlyOutstandingStudentAwardsTab,
+)
 import os
 
 BASE_DIR: str = os.path.dirname(__file__)
@@ -23,53 +27,64 @@ BASE_DIR: str = os.path.dirname(__file__)
 # Function that takes no parameters, and returns nothing
 voidCallBack: Callable[[], None] = lambda: None
 
+
 class ScholarlyTabBar(QTabWidget):
+    """Tab bar for Scholarly app."""
+
     def __init__(
         self,
-        scholarship_tab:ScholarlyScholarshipTab = None,
-        manage_scholarships_tab = None,
-        outstanding_student_awards_tab = None,
-
+        scholarship_tab: ScholarlyScholarshipTab = None,
+        manage_scholarships_tab: ScholarlyManageScholarshipsTab = None,
+        outstanding_student_awards_tab: ScholarlyOutstandingStudentAwardsTab = None,
     ) -> None:
+        """Creates a new instance of ScholarlyTabBar
+
+        Args:
+            scholarship_tab (ScholarlyScholarshipTab, optional): Scholarship tab. Defaults to None.
+            manage_scholarships_tab (ScholarlyManageScholarshipsTab, optional): Manage Scholarships tab. Defaults to None.
+            outstanding_student_awards_tab (ScholarlyOutstandingStudentAwardsTab, optional): Outstanding Student Awards tab. Defaults to None.
+        """
         super().__init__()
 
         # Add scholarship tab
         self.scholarship_tab: ScholarlyScholarshipTab = scholarship_tab
-        self.scholarship_tab_name:str = "Select Scholarship Recipients"
+        self.scholarship_tab_name: str = "Select Scholarship Recipients"
         self.setScholarshipTab(self.scholarship_tab)
 
         # Add manage scholarships tab
         self.manage_scholarships_tab = manage_scholarships_tab
-        self.manage_scholarships_tab_name:str = "Manage Scholarships"
+        self.manage_scholarships_tab_name: str = "Manage Scholarships"
         self.setManageScholarShipsTab(manage_scholarships_tab)
 
         # Add outstanding student awards tab
         self.outstanding_student_awards_tab = outstanding_student_awards_tab
-        self.outstanding_student_awards_tab_name:str = "Outstanding Student Awards"
+        self.outstanding_student_awards_tab_name: str = "Outstanding Student Awards"
         self.setOutstandingStudentAwardTab(self.outstanding_student_awards_tab)
 
-        
-    def setScholarshipTab(self, tab:ScholarlyScholarshipTab)-> None:
+    def setScholarshipTab(self, tab: ScholarlyScholarshipTab) -> None:
         self.removeTab(0)
         self.insertTab(0, tab, self.scholarship_tab_name)
         self.setTabIcon(0, QIcon(os.path.join(BASE_DIR, "assets/icons/education.svg")))
 
-    def setManageScholarShipsTab(self, tab)-> None:
+    def setManageScholarShipsTab(self, tab) -> None:
         self.removeTab(1)
         self.insertTab(1, tab, self.manage_scholarships_tab_name)
-        self.setTabIcon(1, QIcon(os.path.join(BASE_DIR, "assets/icons/filter_settings.svg")))
+        self.setTabIcon(
+            1, QIcon(os.path.join(BASE_DIR, "assets/icons/filter_settings.svg"))
+        )
 
-    def setOutstandingStudentAwardTab(self, tab)-> None:
+    def setOutstandingStudentAwardTab(self, tab) -> None:
         self.removeTab(2)
         self.insertTab(2, tab, self.outstanding_student_awards_tab_name)
         self.setTabIcon(2, QIcon(os.path.join(BASE_DIR, "assets/icons/medal.svg")))
-
 
 
 if __name__ == "__main__":
     from PyQt6.QtWidgets import QApplication
 
     a = QApplication([])
-    s = ScholarlyTabBar(ScholarlyScholarshipTab(), QWidget(), QWidget())
+    s = ScholarlyTabBar(
+        ScholarlyScholarshipTab(), ScholarlyManageScholarshipsTab(), ScholarlyOutstandingStudentAwardsTab()
+    )
     s.show()
     a.exec()
