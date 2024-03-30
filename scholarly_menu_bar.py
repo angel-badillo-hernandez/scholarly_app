9b7aa49203ca92f5a91e742b776cf25542e591d5
Angel Badillo <badilloa022402@gmail.com>
2024-03-29 20:04:15 -0500

    added icons for menu bar

--- scholarly_menu_bar.py
+++ scholarly_menu_bar.py
@@ -1,6 +1,9 @@
 from PyQt6.QtWidgets import QMenuBar, QMenu, QWidget
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QIcon
 from typing import Callable, Any
+import os
+
+BASE_DIR: str = os.path.dirname(__file__)
 
 # Function that takes no parameters, and returns nothing
 voidCallBack: Callable[[], None] = lambda: None
@@ -43,36 +46,44 @@ class ScholarlyMenuBar(QMenuBar):
 
         # Open Action
         self.open_action: QAction = QAction("Open File")
+        self.open_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/open.svg")))
         self.open_action.triggered.connect(open_file_slot)
         self.open_action.setShortcut("ctrl+o")
         self.file_menu.addAction(self.open_action)
 
         # Save Action
         self.save_action: QAction = QAction("Save File", self)
+        self.save_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/save.svg")))
         self.save_action.triggered.connect(save_file_slot)
         self.save_action.setShortcut("ctrl+s")
         self.file_menu.addAction(self.save_action)
 
         # Close Action
         self.close_action: QAction = QAction("Close File", self)
+        self.close_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/close.svg")))
         self.close_action.triggered.connect(close_file_slot)
         self.close_action.setShortcut("ctrl+f4")
         self.file_menu.addAction(self.close_action)
 
         # Exit Action
         self.exit_action: QAction = QAction("Exit", self)
+        self.exit_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/exit.svg")))
         self.exit_action.triggered.connect(exit_slot)
         self.file_menu.addAction(self.exit_action)
 
+        self.help_menu:QMenu = self.addMenu("&Help")
+
         # About Menu Tab
         self.about_action: QAction = QAction("&About", self)
+        self.about_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/about.svg")))
         self.about_action.triggered.connect(about_slot)
-        self.addAction(self.about_action)
+        self.help_menu.addAction(self.about_action)
 
         # Help Menu Tab
         self.help_action: QAction = QAction("&Help", self)
+        self.help_action.setIcon(QIcon(os.path.join(BASE_DIR, "assets/icons/help.svg")))
         self.help_action.triggered.connect(help_slot)
-        self.addAction(self.help_action)
+        self.help_menu.addAction(self.help_action)
 
     def setOpenFileSlot(self, callback: Callable[[QWidget], None]) -> None:
         """Sets the slot for open_file_action.
